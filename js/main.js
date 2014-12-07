aad64fb38561cfcd376cc32950973fa9af997da1
Harrison <hodgesh3@msu.edu>
2014-12-07 06:01:54 -0500

    new pages added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $('.bxslider').bxSlider({
-  mode: 'fade',
   pause: 4000,
   speed: 500,
   autoStart: true,
   startText: "Start",
   stopText: "Stop",
   autoControlsSelector: null,
+  touchEnabled: true,
+  swipeThreshold: 50,
+  preventDefaultSwipeX: true,
   adaptiveHeight: true,
-  responsive: true,
+  responsive: true
 });
