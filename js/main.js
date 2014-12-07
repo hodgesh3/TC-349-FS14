0729cc3651840c422c02692b16a7683384402be0
Harrison <hodgesh3@msu.edu>
2014-12-07 05:14:59 -0500

    sync

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 $('.bxslider').bxSlider({
-  auto: true,
-  autoControls: true
-  autoStart: true
-  startText: "Start"
-  stopText: "Stop"
-  autoControlsSelector: null
-  pause: 4000
+  mode: 'fade',
+  pause: 4000,
+  speed: 500,
+  autoStart: true,
+  startText: "Start",
+  stopText: "Stop",
+  touchEnabled: true,
+  swipeThreshold: 50,
+  preventDefaultSwipeX: true,
   responsive: true
-  touchEnabled: true
-  swipeThreshold: 50
-  preventDefaultSwipeX: true
-  pager: true
-  pagerType: "full"
 });
