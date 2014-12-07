4623c2919a5f504e10b324948d87d871d61afa1d
Harrison <hodgesh3@msu.edu>
2014-12-07 02:44:23 -0500

    upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+$('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true
+  autoStart: true
+  startText: "Start"
+  stopText: "Stop"
+  autoControlsSelector: null
+  pause: 4000
+  responsive: true
+  touchEnabled: true
+  swipeThreshold: 50
+  preventDefaultSwipeX: true
+  pager: true
+  pagerType: "full"
+});
